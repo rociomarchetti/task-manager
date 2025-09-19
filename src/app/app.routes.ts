c319b6ccc7f3ec52718586106e0c71204a37408c
Rocío Marchetti <marchettirociob@gmail.com>
2025-09-19 09:51:34 +0200

    feat(app): add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,12 @@ export const appRoutes: Routes = [
   {
     path: '',
     loadComponent: () => import('./core/layout/layout').then((m) => m.Layout),
-    children: [],
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./features/auth/login.routes').then((m) => m.LOGIN_ROUTES),
+      },
+    ],
   },
 ];
