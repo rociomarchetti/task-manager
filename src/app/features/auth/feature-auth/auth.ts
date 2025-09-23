854d95fe6e5fdc51fd5308f478e018e743321cd9
Rocío Marchetti <marchettirociob@gmail.com>
2025-09-23 09:59:39 +0200

    feat(auth): generate register form component

--- src/app/features/auth/feature-auth/auth.ts
+++ src/app/features/auth/feature-auth/auth.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { LoginFormComponent } from '../ui-login-form/login-form';
+import { RegisterFormComponent } from '../ui-register-form/register-form';
 
 @Component({
   selector: 'app-auth',
-  imports: [LoginFormComponent, MatTabsModule],
+  imports: [LoginFormComponent, RegisterFormComponent, MatTabsModule],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/features/auth/ui-register-form/register-form.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  imports: [],
+  templateUrl: './register-form.html',
+  styleUrl: './register-form.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterFormComponent {}
