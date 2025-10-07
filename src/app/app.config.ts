4cec1a55dbdcf2f486c2024da6f4b9fbac8133a6
Rocío Marchetti <marchettirociob@gmail.com>
2025-10-07 10:13:20 +0200

    feat(domain): set ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,15 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideStore(),
+    provideEffects(),
   ],
 };
--- src/app/features/auth/feature-auth/auth.routes.ts
+++ src/app/features/auth/feature-auth/auth.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+import { AuthFacade } from '../domain/application/auth.facade';
+import { AuthEffects, authReducerFunction } from '../domain/state';
+import { featureKey } from '../domain/state/state/auth.state';
 
 export const AUTH_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./auth').then((m) => m.AuthFeature),
+    providers: [
+      AuthFacade,
+      provideState(featureKey, authReducerFunction),
+      provideEffects([AuthEffects]),
+    ],
   },
 ];
