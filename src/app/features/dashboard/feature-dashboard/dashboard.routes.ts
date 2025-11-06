f22564391373ded19f43b47cf472b3b20ac22872
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-06 11:01:32 +0100

    feat(dashboard): set routes and on init actions

--- src/app/features/dashboard/feature-dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/feature-dashboard/dashboard.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardFacade } from '../domain/application/dashboard.facade';
+import { provideState } from '@ngrx/store';
+import { featureKey } from '../domain/state/state/dashboard.state';
+import { DashboardEffects, dashboardReducerFunction } from '../domain/state';
+import { provideEffects } from '@ngrx/effects';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./dashboard').then((m) => m.DashboardFeature),
-    /*     providers: [
-      AuthFacade,
-      provideState(featureKey, authReducerFunction),
-      provideEffects([AuthEffects]),
-    ], */
+    providers: [
+      DashboardFacade,
+      provideState(featureKey, dashboardReducerFunction),
+      provideEffects([DashboardEffects]),
+    ],
   },
 ];
--- src/app/features/dashboard/feature-dashboard/dashboard.ts
+++ src/app/features/dashboard/feature-dashboard/dashboard.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { DashboardFacade } from './../domain/application/dashboard.facade';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardFeature {}
+export class DashboardFeature implements OnInit {
+  private readonly dashboardFacade = inject(DashboardFacade);
+  readonly viewModel$ = this.dashboardFacade.viewModel$;
+
+  ngOnInit(): void {
+    this.dashboardFacade.viewInitialised();
+  }
+}
