e321e6470da0d73b499176e7992fecf986e1807a
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-06 12:00:33 +0100

    feat(dashboard): add tasks summary component

--- src/app/features/dashboard/feature-dashboard/dashboard.ts
+++ src/app/features/dashboard/feature-dashboard/dashboard.ts
@@ -1,15 +1,16 @@
 import { AsyncPipe } from '@angular/common';
-import { DashboardFacade } from './../domain/application/dashboard.facade';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
+import { DashboardTasksSummary } from '../ui-tasks-summary/dashboard-tasks-summary';
+import { DashboardFacade } from './../domain/application/dashboard.facade';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, DashboardTasksSummary],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/features/dashboard/ui-tasks-summary/dashboard-tasks-summary.ts
+++ src/app/features/dashboard/ui-tasks-summary/dashboard-tasks-summary.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Panel } from '@shared/ui/panel/panel';
+import {
+  PanelFooterDirective,
+  PanelHeaderDirective,
+} from '@shared/ui/panel/panel.directive';
+import { TaskAmounts } from '../domain/entities/dashboard.model';
 
 @Component({
   selector: 'app-dashboard-tasks-summary',
-  imports: [],
+  imports: [Panel, PanelHeaderDirective, PanelFooterDirective],
   templateUrl: './dashboard-tasks-summary.html',
   styleUrl: './dashboard-tasks-summary.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardTasksSummary {}
+export class DashboardTasksSummary {
+  tasks = input<TaskAmounts>();
+}
