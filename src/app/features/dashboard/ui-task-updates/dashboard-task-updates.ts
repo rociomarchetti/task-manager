7e75b839f636ff6acfc94a99285d6a2c7c831d21
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-12 10:46:22 +0100

    feat(dashboard): generate task updates component

---
+++ src/app/features/dashboard/ui-task-updates/dashboard-task-updates.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-task-updates',
+  imports: [],
+  templateUrl: './dashboard-task-updates.html',
+  styleUrl: './dashboard-task-updates.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UiTaskUpdates {}
