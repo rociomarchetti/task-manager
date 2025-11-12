1631ba8de2a3d5674448cbac25ad56f46fb44e1f
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-12 15:38:07 +0100

    feat(dashboard): fix domain utils

--- src/app/features/dashboard/domain/state/util/dashboard.util.ts
+++ src/app/features/dashboard/domain/state/util/dashboard.util.ts
@@ -9,7 +9,10 @@ export const countTasksByStatus = (tasks: Task[]) => ({
 export function getRecentlyCreatedTasks(tasks: Task[]): Task[] {
   return tasks
     ?.slice()
-    .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+    .sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    )
     .slice(0, 3);
 }
 
@@ -17,7 +20,9 @@ export function getUpcomingDueDateTasks(tasks: Task[]): Task[] {
   return tasks
     ?.slice()
     .filter((t) => t.status !== TaskStatus.DONE && !!t.dueDate)
-    .sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime())
+    .sort(
+      (a, b) => new Date(b.dueDate).getTime() - new Date(a.dueDate).getTime()
+    )
     .slice(0, 3);
 }
 
@@ -25,6 +30,9 @@ export function getRecentlyStatusChangedTasks(tasks: Task[]): Task[] {
   return tasks
     ?.slice()
     .filter((t) => !!t.updatedAt)
-    .sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime())
+    .sort(
+      (a, b) =>
+        new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()
+    )
     .slice(0, 3);
 }
