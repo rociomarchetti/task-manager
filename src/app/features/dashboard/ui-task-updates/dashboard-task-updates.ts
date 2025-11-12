8a49ae569f7568b523258c77448d5cf86f497f67
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-12 16:43:58 +0100

    feat(dashboard): change task-upates panel

--- src/app/features/dashboard/ui-task-updates/dashboard-task-updates.ts
+++ src/app/features/dashboard/ui-task-updates/dashboard-task-updates.ts
@@ -1,14 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { Task } from '../domain/entities/dashboard.model';
 import { Panel } from '@shared/ui/panel/panel';
-import {
-  PanelFooterDirective,
-  PanelHeaderDirective,
-} from '@shared/ui/panel/panel.directive';
+import { PanelBodyDirective } from '@shared/ui/panel/panel.directive';
+import { Task } from '../domain/entities/dashboard.model';
 
 @Component({
   selector: 'app-dashboard-task-updates',
-  imports: [Panel, PanelHeaderDirective, PanelFooterDirective],
+  imports: [Panel, PanelBodyDirective],
   templateUrl: './dashboard-task-updates.html',
   styleUrl: './dashboard-task-updates.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
