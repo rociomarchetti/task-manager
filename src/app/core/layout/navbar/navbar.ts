65e68c8f10fbc898614c184d9963108f284a3825
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-12 15:07:00 +0100

    feat(core): add logout draft in navbar

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthFacade } from 'app/features/auth/domain/application/auth.facade';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  private readonly authFacade = inject(AuthFacade);
+
+  logout() {
+    this.authFacade.logout();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { AuthEffects } from './app/features/auth/domain/state/effects/auth.effects';
+import { authReducerFunction } from 'app/features/auth/domain/state';
+import { authFeatureKey } from 'app/features/auth/domain/state/state/auth.state';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    ...appConfig.providers,
+    provideStore({ [authFeatureKey]: authReducerFunction }),
+    provideEffects([AuthEffects]),
+  ],
+}).catch((err) => console.error(err));
