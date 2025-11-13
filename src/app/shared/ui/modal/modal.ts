34e45ea7edfe113fd78fc7f82857be29e11b01d5
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-13 11:25:34 +0100

    feat(shared): generate modal component

---
+++ src/app/shared/ui/modal/modal.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.html',
+  styleUrl: './modal.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Modal {}
