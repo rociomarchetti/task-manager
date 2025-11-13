9f529d541df5558555029dd3af6af10a871e56d7
Rocío Marchetti <marchettirociob@gmail.com>
2025-11-13 11:31:27 +0100

    feat(shared): create shared ui modal

--- src/app/shared/ui/modal/modal.ts
+++ src/app/shared/ui/modal/modal.ts
@@ -1,10 +1,78 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+  output,
+  SimpleChanges,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  imports: [CommonModule, MatDialogModule, MatIconModule],
   templateUrl: './modal.html',
   styleUrl: './modal.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Modal {}
+export class Modal {
+  private readonly dialog = inject(MatDialog);
+
+  isOpen = input(false);
+  isCloseable = input(true);
+  closeModal = output();
+
+  @ViewChild('content') content!: TemplateRef<unknown>;
+
+  private dialogRef?: MatDialogRef<unknown>;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['isOpen']) {
+      this.handleIsOpenChange();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.close();
+  }
+
+  get classes() {
+    return ['modal', this.isCloseable() ? 'modal--is-closable' : ''];
+  }
+
+  handleIsOpenChange() {
+    if (!this.content) return;
+    if (this.isOpen()) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
+
+  open(): void {
+    if (this.content) {
+      this.dialogRef = this.dialog.open(this.content, {
+        disableClose: !this.isCloseable(),
+        backdropClass: 'modal__backdrop',
+        panelClass: this.classes,
+        maxHeight: '85vh',
+        autoFocus: false,
+      });
+      this.dialogRef.afterClosed().subscribe((result) => {
+        this.closeModal.emit(result);
+      });
+    }
+  }
+
+  close(): void {
+    this.dialogRef?.close();
+  }
+}
