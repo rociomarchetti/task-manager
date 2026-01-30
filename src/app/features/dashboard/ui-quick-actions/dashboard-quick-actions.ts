785d38c9985cbc50df81dd4e53b9734f6d05f7cc
Rocio Marchetti <marchettirociob@gmail.com>
2026-01-30 21:39:14 +0100

    feat(dashboard): generate quick-actions component

---
+++ src/app/features/dashboard/ui-quick-actions/dashboard-quick-actions.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-quick-actions',
+  imports: [],
+  templateUrl: './dashboard-quick-actions.html',
+  styleUrl: './dashboard-quick-actions.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardQuickActions {}
