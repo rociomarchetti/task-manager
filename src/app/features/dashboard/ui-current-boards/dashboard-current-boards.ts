e6b7cf8a7e50d9c87b98312512a5da15565b66d3
Rocio Marchetti <marchettirociob@gmail.com>
2026-01-30 17:25:53 +0100

    feat(dashboard): generate current-boards component

---
+++ src/app/features/dashboard/ui-current-boards/dashboard-current-boards.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-current-boards',
+  imports: [],
+  templateUrl: './dashboard-current-boards.html',
+  styleUrl: './dashboard-current-boards.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardCurrentBoards {}
