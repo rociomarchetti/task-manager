fd2e040ff8d15bde651394af4136b966029ed2c1
Rocio Marchetti <marchettirociob@gmail.com>
2026-02-11 08:18:51 -0300

    feat(dashboard): fix lint warning error

--- src/app/features/dashboard/ui-task-summary/dashboard-task-summary.ts
+++ src/app/features/dashboard/ui-task-summary/dashboard-task-summary.ts
@@ -1,14 +1,10 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TaskAmounts } from '@shared/models';
 import { Panel } from '@shared/ui/panel/panel';
-import {
-  PanelFooterDirective,
-  PanelHeaderDirective,
-} from '@shared/ui/panel/panel.directive';
 
 @Component({
   selector: 'app-dashboard-task-summary',
-  imports: [Panel, PanelHeaderDirective, PanelFooterDirective],
+  imports: [Panel],
   templateUrl: './dashboard-task-summary.html',
   styleUrl: './dashboard-task-summary.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
