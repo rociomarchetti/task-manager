74bb8e32b0b99a2ede6c3021b69fce55e176f830
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-03 11:18:51 +0100

    feat(core): change boards mock

--- src/app/core/services/boards-service/__mocks__/mock-boards.ts
+++ src/app/core/services/boards-service/__mocks__/mock-boards.ts
@@ -3,7 +3,7 @@ import { Board } from '@shared/models';
 export const mockBoards: Board[] = [
   {
     id: 'A',
-    name: 'Crear el gestor de tareas',
+    title: 'Crear el gestor de tareas',
     createdAt: new Date('2026-01-27T10:00:00Z'),
     isFavorite: true,
     isWip: true,
@@ -11,7 +11,7 @@ export const mockBoards: Board[] = [
   },
   {
     id: 'B',
-    name: 'Mantenimiento del mes',
+    title: 'Mantenimiento del mes',
     createdAt: new Date('2026-01-27T10:00:00Z'),
     isFavorite: false,
     isWip: true,
