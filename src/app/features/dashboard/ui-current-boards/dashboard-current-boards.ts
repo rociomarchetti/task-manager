6196a3b5c91cc1ead1743ceb40243b8864c9a205
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-23 09:46:36 +0100

    feat(dashboard): add new ui components

--- src/app/features/dashboard/ui-current-boards/dashboard-current-boards.ts
+++ src/app/features/dashboard/ui-current-boards/dashboard-current-boards.ts
@@ -5,22 +5,30 @@ import {
   output,
 } from '@angular/core';
 import { Board } from '@shared/models';
+import { List } from '@shared/ui/list/list';
 import { Panel } from '@shared/ui/panel/panel';
 import { PanelBodyDirective } from '@shared/ui/panel/panel.directive';
+import { mockBoards } from 'app/core/services/boards-service/__mocks__/mock-boards';
+import { BoardListItem } from 'app/features/shared/board-list-item/board-list-item';
 
 @Component({
   selector: 'app-dashboard-current-boards',
-  imports: [Panel, PanelBodyDirective],
+  imports: [BoardListItem, List, Panel, PanelBodyDirective],
   templateUrl: './dashboard-current-boards.html',
   styleUrl: './dashboard-current-boards.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardCurrentBoards {
+  mockBoards = mockBoards;
   currentBoards = input<Array<Board>>();
 
   goToBoardClicked = output<Board>();
 
   onGoToBoard(board: Board): void {
     this.goToBoardClicked.emit(board);
+  }
+
+  onActionClicked(actionId: string) {
+    console.log(actionId);
   }
 }
