22fb047c541c86917a3dce71653a2640a1bd53e5
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-23 09:44:13 +0100

    feat(shared): add board list item first draft

--- src/app/features/shared/board-list-item/board-list-item.ts
+++ src/app/features/shared/board-list-item/board-list-item.ts
@@ -1,10 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { Board } from '@shared/models';
+import { QuickAction } from '@shared/models/quick-action.model';
+import { ListItem } from '@shared/ui/list-item/list-item';
 
 @Component({
   selector: 'app-board-list-item',
-  imports: [],
+  imports: [ListItem],
   templateUrl: './board-list-item.html',
   styleUrl: './board-list-item.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BoardListItem {}
+export class BoardListItem {
+  board = input<Board>();
+
+  seeBoard = output<Board>();
+  removeBoard = output<Board>();
+
+  get quickActions(): Array<QuickAction> {
+    return [
+      {
+        id: 'see',
+        icon: 'visibility',
+        label: 'Ver',
+      },
+      {
+        id: 'remove',
+        icon: 'delete',
+        label: 'Eliminar',
+      },
+    ];
+  }
+
+  onActionClicked(actionId: string) {
+    console.log(actionId);
+    switch (actionId) {
+      case 'see':
+        this.onSeeBoardClicked();
+        break;
+
+      case 'remove':
+        this.onRemoveBoardClicked();
+        break;
+    }
+  }
+
+  onSeeBoardClicked() {
+    this.seeBoard.emit(this.board());
+  }
+
+  onRemoveBoardClicked() {
+    this.removeBoard.emit(this.board());
+  }
+}
