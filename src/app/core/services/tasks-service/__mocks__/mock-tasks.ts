7bf57bb81126e28666bb53b414632b5c1835075e
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-23 17:47:17 +0100

    feat(core): update tasks mock

--- src/app/core/services/tasks-service/__mocks__/mock-tasks.ts
+++ src/app/core/services/tasks-service/__mocks__/mock-tasks.ts
@@ -1,5 +1,7 @@
 import { Task, TaskStatus } from '@shared/models';
 
+const now = new Date();
+
 export const mockTasks: Task[] = [
   {
     id: '1',
@@ -7,7 +9,7 @@ export const mockTasks: Task[] = [
     title: 'Diseñar la página principal',
     description: 'Crear el diseño inicial del landing page en Figma',
     status: TaskStatus.IN_PROGRESS,
-    createdAt: new Date('2026-01-27T10:00:00Z'), // Creada hace 2 días
+    createdAt: new Date('2026-01-27T10:00:00Z'),
     updatedAt: new Date('2026-01-27T14:30:00Z'),
     dueDate: new Date('2026-02-15T00:00:00Z'),
     userId: 1,
@@ -19,7 +21,7 @@ export const mockTasks: Task[] = [
     description:
       'Instalar dependencias y configurar el entorno local de React + Vite',
     status: TaskStatus.PENDING,
-    createdAt: new Date('2026-01-28T08:00:00Z'), // Creada hace 1 día
+    createdAt: new Date('2026-01-28T08:00:00Z'),
     dueDate: new Date('2026-02-10T00:00:00Z'),
     userId: 1,
   },
@@ -30,7 +32,7 @@ export const mockTasks: Task[] = [
     description: 'Agregar login y registro con JWT y refresh tokens',
     status: TaskStatus.IN_PROGRESS,
     createdAt: new Date('2026-01-15T09:00:00Z'),
-    updatedAt: new Date('2026-01-28T16:45:00Z'), // Actualizada hace 1 día
+    updatedAt: new Date('2026-01-28T16:45:00Z'),
     dueDate: new Date('2026-02-20T00:00:00Z'),
     userId: 1,
   },
@@ -41,8 +43,8 @@ export const mockTasks: Task[] = [
     description: 'Generar documentación con Swagger y ejemplos de uso',
     status: TaskStatus.IN_PROGRESS,
     createdAt: new Date('2026-01-10T11:30:00Z'),
-    updatedAt: new Date('2026-01-27T10:00:00Z'), // Actualizada hace 2 días
-    dueDate: new Date('2026-02-25T00:00:00Z'),
+    updatedAt: new Date('2026-01-27T10:00:00Z'),
+    dueDate: new Date(now.getTime() + 2 * 24 * 60 * 60 * 1000), // +2 días
     userId: 1,
   },
   {
@@ -53,7 +55,7 @@ export const mockTasks: Task[] = [
     status: TaskStatus.IN_PROGRESS,
     createdAt: new Date('2026-01-20T16:00:00Z'),
     updatedAt: new Date('2026-01-25T12:00:00Z'),
-    dueDate: new Date('2026-01-31T00:00:00Z'), // Vence en 2 días
+    dueDate: new Date(now.getTime() + 4 * 24 * 60 * 60 * 1000), // +4 días
     userId: 1,
   },
   {
@@ -64,7 +66,7 @@ export const mockTasks: Task[] = [
     status: TaskStatus.PENDING,
     createdAt: new Date('2026-01-22T09:30:00Z'),
     updatedAt: new Date('2026-01-26T18:45:00Z'),
-    dueDate: new Date('2026-01-30T00:00:00Z'), // Vence mañana
+    dueDate: new Date(now.getTime() + 1 * 24 * 60 * 60 * 1000), // +1 día
     userId: 1,
   },
 ];
