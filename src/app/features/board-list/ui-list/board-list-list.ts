946fec99e4f5cce28fcd37eca9f210c7acb17a70
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-30 14:05:01 +0200

    feat(board-list): generate list component

---
+++ src/app/features/board-list/ui-list/board-list-list.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-list-list',
+  imports: [],
+  templateUrl: './board-list-list.html',
+  styleUrl: './board-list-list.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BoardListList {}
