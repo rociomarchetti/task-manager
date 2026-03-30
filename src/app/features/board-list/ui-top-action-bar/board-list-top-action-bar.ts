9cc16c205cffcbd632b25267a80d70012ec97ef2
Rocio Marchetti <marchettirociob@gmail.com>
2026-03-30 14:01:08 +0200

    feat(board-list): generate top action bar component

---
+++ src/app/features/board-list/ui-top-action-bar/board-list-top-action-bar.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-list-top-action-bar',
+  imports: [],
+  templateUrl: './board-list-top-action-bar.html',
+  styleUrl: './board-list-top-action-bar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BoardListTopActionBar {}
