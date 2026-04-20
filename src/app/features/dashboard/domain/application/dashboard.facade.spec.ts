97c0db934089ec24a2859ee45cd5b9fe15b5c371
Rocio Marchetti <marchettirociob@gmail.com>
2026-04-20 17:04:22 +0200

    feat(dashboard): facade tests

---
+++ src/app/features/dashboard/domain/application/dashboard.facade.spec.ts
@@ -0,0 +1,157 @@
+import { TestBed } from '@angular/core/testing';
+import { DashboardFacade } from './dashboard.facade';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideZoneChangeDetection } from '@angular/core';
+import * as fromActions from '../state/actions/dashboard.actions';
+import { NewTaskData, Task } from '@shared/models';
+
+describe('GIVEN: Dashboard Facade', () => {
+  let store: MockStore;
+  let facade: DashboardFacade;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        DashboardFacade,
+        provideMockStore(),
+        provideZoneChangeDetection({ ignoreChangesOutsideZone: true }),
+      ],
+    });
+  });
+
+  beforeEach(() => {
+    store = TestBed.inject(MockStore);
+    facade = TestBed.inject(DashboardFacade);
+  });
+
+  describe('WHEN: viewInitialised', () => {
+    it('THEN: should dispatch viewInitialised action', () => {
+      const action = fromActions.DashboardViewActions.viewInitialised();
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.viewInitialised();
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: markTaskAsDone', () => {
+    it('THEN: should dispatch markTaskAsDone action', () => {
+      const mockTaskId = 'A';
+      const action = fromActions.DashboardTaskActions.completedTask({
+        taskId: mockTaskId,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.markTaskAsDone(mockTaskId);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: postponeTask', () => {
+    it('THEN: should dispatch postponeTask action', () => {
+      const mockTaskId = 'A';
+      const action = fromActions.DashboardTaskActions.postponedTask({
+        taskId: mockTaskId,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.postponeTask(mockTaskId);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: goToBoard', () => {
+    it('THEN: should dispatch goToBoard action', () => {
+      const mockBoardId = 'A';
+      const action = fromActions.DashboardBoardActions.goToBoardClicked({
+        boardId: mockBoardId,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.goToBoard(mockBoardId);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: removeBoard', () => {
+    it('THEN: should dispatch removeBoard action', () => {
+      const mockBoardId = 'A';
+      const action = fromActions.DashboardBoardActions.removeBoardClicked({
+        boardId: mockBoardId,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.removeBoard(mockBoardId);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: createNewBoard', () => {
+    it('THEN: should dispatch createNewBoard action', () => {
+      const action = fromActions.DashboardBoardActions.createNewBoardClicked();
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.createNewBoard();
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: createNewTask', () => {
+    it('THEN: should dispatch createNewTask action', () => {
+      const mockNewTask = {} as NewTaskData;
+      const action = fromActions.DashboardTaskActions.createNewTaskClicked({
+        newTask: mockNewTask,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.createNewTask(mockNewTask);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: updateTask', () => {
+    it('THEN: should dispatch updateTask action', () => {
+      const mockTask = {} as Task;
+      const action = fromActions.DashboardTaskActions.editTask({
+        updatedTask: mockTask,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.updateTask(mockTask);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: removeTask', () => {
+    it('THEN: should dispatch removeTask action', () => {
+      const mockTaskId = 'A';
+      const action = fromActions.DashboardTaskActions.removeTaskClicked({
+        taskId: mockTaskId,
+      });
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.removeTask(mockTaskId);
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('WHEN: goToBoardsList', () => {
+    it('THEN: should dispatch goToBoardsList action', () => {
+      const action = fromActions.DashboardBoardActions.goToBoardsListClicked();
+      const result = jest.spyOn(store, 'dispatch');
+
+      facade.goToBoardsList();
+
+      expect(result).toHaveBeenCalledWith(action);
+    });
+  });
+});
