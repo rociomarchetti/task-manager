f34e07ac76b1255ba6fd18dd2fac1c5a8a1aabd9
Rocio Marchetti <marchettirociob@gmail.com>
2026-04-25 12:59:18 +0200

    feat(core): navbar styles refactor

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -4,9 +4,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthFacade } from 'app/features/auth/domain/application/auth.facade';
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { User } from '@shared/models';
 @Component({
   selector: 'app-navbar',
-  imports: [AsyncPipe, MatButtonModule, MatToolbarModule, RouterModule],
+  imports: [
+    AsyncPipe,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    RouterModule,
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
@@ -16,5 +24,10 @@ export class Navbar {
 
   logout() {
     this.authFacade.logout();
+  }
+
+  getUserName(user: User): string {
+    const message = `Hola ${user.name} ${user.lastName}`;
+    return message;
   }
 }
