0bcd4368b67706c0d5d7085a9c4bed7813fc79de
Rocio Marchetti <marchettirociob@gmail.com>
2026-04-27 14:43:13 +0200

    feat(board-list): add tooltip and modal confirmation

--- src/app/features/board-list/ui-list/board-list-list.ts
+++ src/app/features/board-list/ui-list/board-list-list.ts
@@ -1,18 +1,26 @@
+import { ConfirmationModal } from '@shared/features/confirmation-modal/confirmation-modal';
 import {
   ChangeDetectionStrategy,
   Component,
   computed,
   input,
   output,
+  signal,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Board } from '@shared/models';
 import { formatDate } from '@shared/features/util/formatDate.util';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-board-list-list',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [
+    ConfirmationModal,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
   templateUrl: './board-list-list.html',
   styleUrl: './board-list-list.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,6 +32,9 @@ export class BoardListList {
 
   goToBoardDetails = output<string>();
   removeBoard = output<string>();
+
+  isModalOpen = signal(false);
+  selectedBoard = signal<Board | null>(null);
 
   boardsToShow = computed(() => {
     const { search, favorites } = this.filters() ?? {
@@ -41,7 +52,18 @@ export class BoardListList {
     this.goToBoardDetails.emit(boardId);
   }
 
-  onRemoveBoard(boardId: string): void {
-    this.removeBoard.emit(boardId);
+  onModalClosed(): void {
+    this.isModalOpen.set(false);
+  }
+
+  onModalConfirmDeletion(): void {
+    const selectedBoardId = this.selectedBoard()?.id;
+    this.removeBoard.emit(selectedBoardId!);
+    this.isModalOpen.set(false);
+  }
+
+  onRemoveBoard(board: Board): void {
+    this.selectedBoard.set(board);
+    this.isModalOpen.set(true);
   }
 }
